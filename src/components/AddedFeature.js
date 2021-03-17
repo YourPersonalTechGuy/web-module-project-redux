30d74463f804abd8697d1d7e0171b10451e3b79f
Anas Abdelsalam <anas.abdelsalam57@gmail.com>
2021-03-16 21:23:01 -0500

    MVP Completed

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { removeFeature } from '../actions';
+import { useDispatch } from "react-redux";
 
 const AddedFeature = props => {
+
+  const dispatch = useDispatch()
+
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button className="button" onClick={()=>{dispatch(removeFeature(props.feature))}}>X</button>
       {props.feature.name}
     </li>
   );
 };
+
 
 export default AddedFeature;
